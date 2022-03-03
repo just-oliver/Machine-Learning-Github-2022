234c6b47ffbaefed22471b8382e74cf2247763a4
txh893 <47370061+txh893@users.noreply.github.com>
2022-03-03 17:25:30 +0000

    Add files via upload

---
+++ Acrobot_ActorCritic.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import gym
+import numpy as np
+from ActorCritic import Agent
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    env = gym.make("Acrobot-v1")
+    agent = Agent(alpha=1e-5, n_actions=env.action_space.n)
+    n_games = 100
+    
+    best_score = env.reward_range[0]
+    score_history = []
+    load_checkpoint = True
+    time_limit = 500
+    
+    if load_checkpoint:
+        agent.load_models()
+    
+    for i in range(n_games):
+        observation = env.reset()
+        score = 0
+        for timestep in range(1, time_limit):
+            action = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            
+            reward = 1 - observation_[0]
+            if done and timestep < time_limit - 10:
+                reward += 10 * (time_limit - timestep)
+            score += reward
+            if not load_checkpoint:
+                agent.learn(observation, reward, observation_, done)
+            observation = observation_
+            if done:
+                break
+        score_history.append(score)
+        avg_score = np.mean(score_history[-10:])
+            
+        if avg_score > best_score:
+            best_score = avg_score
+            if not load_checkpoint:
+                agent.save_models()
+        print("episode ", i, "score %.1f" % score, "avg_score %.1f" % avg_score)
+     
+    x = [i+1 for i in range(n_games)]
+    plt.plot(x, score_history)
+
